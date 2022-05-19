55f49be2d31665716a49d6af2625a8bb92541d4e
Diego <Diegoarod14@gmail.com>
2022-05-18 23:09:13 -0400

    Added Nav bar, theme still to be created as well as logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Route, BrowserRouter as Router, Routes, Link} from 'react-router-dom';
+import Home from './Containers/Home/Home';
+import Services from './Containers/Services/Services';
+import Contact from './Containers/Contact/Contact';
+import Navbar from './Components/Navbar/Navbar'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path = "/" component = {Home}/>
+          <Route exact path = "/Services" component = {Services}/>
+          <Route exact path = "/Contact" component = {Contact}/>
+        </Routes>
+
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar/styles.js
@@ -0,0 +1,21 @@
+//Styles used by MUI components
+
+
+
+export const buttonStyles = {
+    my: 2,
+    color: 'white',
+    display: 'block',
+    '&:hover':{
+        background: "rgba(0, 0, 0, 0.87)"
+    }
+}
+
+export const boxStyles = {
+    flexGrow: 1, 
+    display: { xs: 'none', md: 'flex' },
+    background:"#1976d2",
+    
+}
+
+// "color: rgba(0, 0, 0, 0.87)"
