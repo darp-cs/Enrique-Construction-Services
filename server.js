149214ca59f36be4a22af51034560bc0601e0716
Diego <Diegoarod14@gmail.com>
2022-07-09 20:06:11 -0400

    Finished the server, need to make changes to package to connect backend and front end in live env

--- server.js
+++ server.js
@@ -17,51 +17,71 @@ const profile = {
 }
 
 
-const transporter = nodemailer.createTransport(profile)
 
-.
-transporter.verify((error, success) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Credential Login Worked!');
-    }
-  });
 
 
 
 const app = express();
 app.use(express.json())
-    app.post('contact/send', (req, res) => {
+
+
+
+//Used to let Node serve our files for out built react app
+app.use(express.static(path.join(__dirname,'/build')));
+
+//post request for contact form
+app.post('/contact/send', (req, res) => {
+    console.log('inside post ')
     const name = req.body.name
     const email = req.body.email
-    const message = req.body.messageHtml
-    
-    
-    var mail = {
-        from: name,
-        to: process.env.USER,
-        subject: 'Contact form request',
-        html: message
-    }
-  
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        res.json({
-          msg: 'fail'
-        })
-      } else {
-        res.status(201)
-      }
-    })
-})
+    const message = req.body.message
+    const contactNum = (req.body.contactNum==null)?'No contact number provided':req.body.contactNum
 
-app.use(express.static(path.join(__dirname,'/build')));
+    const content = {
+        from: process.env.USER,
+        to: process.env.USER,
+        subject: `Contact form request from ${name}`,
+        text: message,
+        html: `<h3>From: ${name}</h3>
+        <h3>Email: ${email}</h3>
+        <h3>Contact Number: ${contactNum}</h3>
+        <h4> ${message}</h4>`
+    }
+    const transporter = nodemailer.createTransport(profile)
+    console.log(`request: ${JSON.stringify(req.body)}`)
+    transporter.verify((error, success) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Credential Login Worked!');
+        }
+      });
+    transporter.sendMail(content, (err, info) => {
+        if (err) {
+            res.json({
+                msg: 'fail'
+              })
+          } else {
+            res.json({
+                msg: 'success'
+              })
+            // res.status(200);
+          }
+    });
+    transporter.close()
+    })
+
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+    });
+
+//get requests not handled will be sent to react app
 app.get('*',(req, res)=>{
     res.sendFile(path.join(__dirname,'build/index.html'))
 })
 
-app.listen(process.env.PORT || 5000, () =>{
+app.listen(process.env.API_PORT || 5000, () =>{
     console.log(`Server is running on local host: ${process.env.API_PORT}`)
 })
 
