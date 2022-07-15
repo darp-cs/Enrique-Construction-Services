b5261c450f72e4c5e8e2ed5a278340e0fbac1719
Diego Rodriguez <Diego@Marianas-MacBook-Pro.local>
2022-07-15 18:42:05 -0400

    Updated

---
+++ src/Containers/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () =>{
+    <div>{console.log('Home')}
+        <div>HOME</div>
+    </div>
+}
+
+export default Home;
---
+++ src/Containers/Services.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Services = () =>{
+    <div>{console.log('Services')}
+    <div>Services</div>
+    </div>
+}
+
+export default Services;
